471127d9f5458d5f1289fac723f92addb8b9c8a1
GeoffreyHW <wonggh97@gmail.com>
2018-04-25 13:32:52 -0700

    first

---
+++ client/query.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+def get_vendors():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        print("The number of parts: ", cur.rowcount)
+        rows = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return rows
