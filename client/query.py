2f2e906c5ba5cfd5041783143dcb1f14fc1d7188
GeoffreyHW <wonggh97@gmail.com>
2018-04-25 14:06:20 -0700

    querydb

--- client/query.py
+++ client/query.py
@@ -1,10 +1,14 @@
+from flask import Flask
 import psycopg2
 
+APP=Flask(__name__)
+
+@APP.route('/querydb', methods=['GET'])
 def get_vendors():
     """ query data from the vendors table """
     conn = None
     try:
-        conn = psycopg2.connect(database="testdb", user = "postgres", password = "pass123", host = "127.0.0.1", port = "5432")
+        conn = psycopg2.connect(database="outfile", user = "postgres", password = "pass123", host = "127.0.0.1", port = "5432")
         cur = conn.cursor()
         cur.execute ("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
         print("The number of parts: ", cur.rowcount)
